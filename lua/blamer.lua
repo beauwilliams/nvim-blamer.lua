1f9ab7bfd12f0d695ce675570284a45786193890
Beau <beau.williams@icloud.com>
2020-12-19 16:11:38 +1100

    Adding a nerdfont Icon. Class: nf-oct-git_commit
    I like this icon. It looks nice and unassuming. Helps separate commit blames from code comments.

--- lua/blamer.lua
+++ lua/blamer.lua
@@ -21,14 +21,14 @@ function M.blameVirtText()
   local line = api.nvim_win_get_cursor(0)
   local blame = vim.fn.system(string.format('git blame -c -L %d,%d %s', line[1], line[1], currFile))
   local hash = vim.split(blame, '%s')[1]
-  local cmd = string.format("git show %s ", hash).."--format='🐛 %an | %ar | %s'"
+  local cmd = string.format("git show %s ", hash).."--format='  %an | %ar | %s'"
   if hash == '00000000' then
-    text = 'Not Committed Yet'
+    text = '  Not Committed Yet'
   else
     text = vim.fn.system(cmd)
     text = vim.split(text, '\n')[1]
     if text:find("fatal") then -- if the call to git show fails
-      text = 'Not Committed Yet'
+      text = '  Not Committed Yet'
     end
   end
   api.nvim_buf_set_virtual_text(0, 2, line[1] - 1, {{ text,'GitLens' }}, {}) -- set virtual text for namespace 2 with the content from git and assign it to the higlight group 'GitLens'
